b25d321e4fc233495c294d13a7003b2c5d0bf97d
fernando_uf <nando91@usp.br>
2025-08-25 10:18:41 -0300

    Chamada da função plot_W0_x_Sw na main

--- entrega_desempenho.py
+++ entrega_desempenho.py
@@ -11,3 +11,17 @@ dt.geometry(airplane)
 
 Swvec = np.arange(50, 151, 1)
 m.plot_T0_x_Sw(airplane, Swvec)
+
+sweep_wing_v = np.arange(15,27.5,2.5)*np.pi/180
+sweep_w = airplane['sweep_w']
+sweep_wing_v = np.append(sweep_wing_v, sweep_w)
+
+flap_type_v = ['plain','single slotted','double slotted', 'triple slotted']
+slat_type_v = [None, 'leading edge flap', 'Kruger flaps','slats']
+#print(flap_type_v)
+#print(slat_type_v)
+#print(sweep_wing_v)
+
+m.plot_W0_x_Sw(airplane, Swvec, sweep_wing_v)
+
+# m.plot_W0_x_Sw(airplane, Swvec, sweep_wing_v, flap_type_v, slat_type_v)
