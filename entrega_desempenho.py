3fca38e829397837f18947d9c5a84a6af3423a59
fernando_uf <nando91@usp.br>
2025-08-28 21:46:00 -0300

    Updating main code with new plots for Question 3

--- entrega_desempenho.py
+++ entrega_desempenho.py
@@ -9,19 +9,22 @@ airplane = dt. standard_airplane ('my_airplane_1')
 dt.geometry(airplane)
 # dt.plot3d(airplane)
 
-Swvec = np.arange(50, 151, 1)
+Swvec = np.arange(50, 151, 5) # Cria vetor de areas de asa 
+
+### Questão 1 ###
 m.plot_T0_x_Sw(airplane, Swvec)
 
-sweep_wing_v = np.arange(15,27.5,2.5)*np.pi/180
-sweep_w = airplane['sweep_w']
-sweep_wing_v = np.append(sweep_wing_v, sweep_w)
+### Questão 3 ###
+ 
+sweep_wing_v = np.arange(14,32,2)*np.pi/180         # cria vetor de angulos de enflechamento
+sweep_w = airplane['sweep_w']                       # puxa o enflechamento do nosso avião no dicionário e salva em uma variavel
+sweep_wing_v = np.append(sweep_wing_v, sweep_w)     # adiciona o enflechamento do nosso avião no vetor de enflechamentos
 
-flap_type_v = ['plain','single slotted','double slotted', 'triple slotted']
-slat_type_v = [None, 'leading edge flap', 'Kruger flaps','slats']
-#print(flap_type_v)
-#print(slat_type_v)
-#print(sweep_wing_v)
+# slat_type_v = [None, 'leading edge flap', 'Kruger flaps','slats']               # lista com tipos de config de slat
+flap_type_v = ['plain','single slotted','double slotted', 'triple slotted']       # lista com tipos de config de flap
 
-m.plot_W0_x_Sw(airplane, Swvec, sweep_wing_v)
+## Plot de W0 x Sw com config de flap variando para cada sweep 
+m.plot_W0_x_Sw(airplane, Swvec, sweep_wing_v, flap_type_v)
 
-# m.plot_W0_x_Sw(airplane, Swvec, sweep_wing_v, flap_type_v, slat_type_v)
+## Plot de W0 x Sw com sweep variando
+m.plot_W0_x_sweep(airplane, Swvec, sweep_wing_v)
