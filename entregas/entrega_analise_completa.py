9b7c8994b4e5258b334e86be6d0c6bf44f58c5c6
israelprospero <israelprospero@gmail.com>
2025-09-12 18:13:35 -0300

    Create entrega_analise_completa.py

---
+++ entregas/entrega_analise_completa.py
@@ -0,0 +1,25 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from modules import designTool as dt
+import matplotlib . pyplot as plt
+import numpy as np
+import pprint
+from modules import utils as m
+
+airplane = dt. standard_airplane ('my_airplane_1')
+dt.geometry(airplane)
+# dt.plot3d(airplane)
+
+W0_guess = 50150*dt.gravity
+T0_guess = 0.3*W0_guess 
+
+dt.thrust_matching(W0_guess, T0_guess, airplane)
+dt.balance(airplane)
+dt.landing_gear(airplane)
+
+print("airplane['frac_nlg_fwd'] =", airplane['frac_nlg_fwd'])
+print("airplane['frac_nlg_aft'] =", airplane['frac_nlg_aft'])
+print("airplane['alpha_tipback'] =", airplane['alpha_tipback'])
+print("airplane['alpha_tailstrike'] =", airplane['alpha_tailstrike'])
+print("airplane['phi_overturn'] (deg) =", airplane['phi_overturn']*180/np.pi)
